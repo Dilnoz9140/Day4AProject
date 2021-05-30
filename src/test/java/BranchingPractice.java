61e5901230a4d1df6efa9b92e3adc71b61589c8f
Dilnoza <dilnoz8605@icloud.com>
2021-05-30 10:59:09 +0500

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,10 @@
 public class BranchingPractice {
     public static void main(String[] args) {
+        //created new project
+        // enabled version control
+        //added.gitignore.....
+        //made initial commit
+        //shared project on github
 
     }
 }
